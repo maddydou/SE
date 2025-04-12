eac3e3bb243d5adc13a246dff342e3936932dcc7
JoeyLeder <jleder@uark.edu>
2025-04-11 19:11:17 -0500

    Add files via upload

--- python_udpserver.py
+++ python_udpserver.py
@@ -15,8 +15,10 @@ UDPServerSocket.bind((localIP, localPort))
 
 print("UDP server up and listening")
 
-# Listen for incoming datagrams
+playerscorelist = [] # format: { equipment id, hit other base?, score }
+# initialize array
 
+# Listen for incoming datagrams
 while(True):
 	
 	bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
@@ -31,6 +33,11 @@ while(True):
 	cMsg = clientMsg[22:]
 	cMsg = cMsg[:-1]
 	
+	equipment = True
+	for i in range(len(cMsg)):
+		if cMsg[i] == ':':
+			equipment = False
+	
 	if cMsg == "Hello UDP Server":
 		print("Hello UDP Client")
 	elif cMsg == '202':
@@ -38,7 +45,33 @@ while(True):
 	elif cMsg == '221' or cMsg == '222':
 		print("Closing Server...")
 		break
+	elif equipment:
+		playerscorelist.append({ cMsg, False, 0 })
 	else:
+		equipment = True
+		
 		cMsgNum1 = int(cMsg[:-3])
 		cMsgNum2 = int(cMsg[3:])
-		UDPServerSocket.sendto(str.encode(str(random.randint(203,221))), address)
+		
+		if cMsgNum2 == 53 and cMsgNum1 % 2 == 1:
+			# code for Red team hitting other base
+			for i in range(len(playerscorelist)):
+				if playerscorelist[i][0] == cMsgNum1:
+					playerscorelist[i][1] == true
+		elif cMsgNum2 == 43 and cMsgNum1 % 2 == 0:
+			# code for Green team hitting other base
+			for i in range(len(playerscorelist)):
+				if playerscorelist[i][0] == cMsgNum1:
+					playerscorelist[i][1] == true
+		elif (cMsgNum1 % 2 == 0 and cMsgNum2 % 2 == 0) or (cMsgNum1 % 2 == 1 and cmsgNum2 % 2 == 1):
+			# code for deducting 10 points from cMsgNum1 player
+			for i in range(len(playerscorelist)):
+				if playerscorelist[i][0] == cMsgNum1:
+					if playerscorelist[i][2] != 0:
+						playerscorelist[i][2] == playerscorelist[i][2] - 10;
+		else:
+			# code for adding 10 points to cMsgNum1 player
+			for i in range(len(playerscorelist)):
+				if playerscorelist[i][0] == cMsgNum1:
+					playerscorelist[i][2] == playerscorelist[i][2] + 10;
+		# UDPServerSocket.sendto(str.encode(str(random.randint(203,221))), address)
